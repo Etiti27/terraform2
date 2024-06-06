8edcb59b274168af9af17ac4003ea534db5677b5
etiti <christopherobinna27@gmail.com>
2024-06-06 23:44:03 +0200

    s3 bucket commented file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "orneltchriswesdd"
-    key    = "chris/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "orneltchriswesdssd"
+#     key    = "chris/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
